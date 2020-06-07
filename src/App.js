c73ddb5131f41558cf4b7948474b1f9993b9797c
Ilya Gotfryd <ilya.gotfryd@gmail.com>
2020-06-06 23:01:18 -0400

    :sparkles: adding cleaning time and status display

--- src/App.js
+++ src/App.js
@@ -20,25 +20,37 @@ class App extends Component{
     // if there is no event add one that was an hour ago
     // else return one for this space
     if (!this._cleaning_events[space_id]){
-      this._cleaning_events[space_id] = {time: new Date(demo_now().getTime() - 60*60*1000)}
+      let start_time = new Date(demo_now().getTime() - 3*60*60*1000)
+      let done_time = new Date(demo_now().getTime() - 3*60*60*1000)
+      this._cleaning_events[space_id] = {time: done_time, start_time: start_time };
     }
     return this._cleaning_events[space_id]
   }
 
   addSpaceCleanedEvent(space_id){
-    this._cleaning_events[space_id] = {time: new Date(demo_now() - 1000) }
+    this._cleaning_events[space_id] = {...this._cleaning_events[space_id], time: new Date(demo_now() - 1000), cleaningInProgress: false }
+  }
+
+  addStartCleaningEvent(space_id){
+    this._cleaning_events[space_id] = {...this._cleaning_events[space_id], start_time: demo_now() , cleaningInProgress: true }
+    return this._cleaning_events[space_id];
   }
 
   fetchEntrCounts(space){
       let last_cleaned_event = this.getLatestSpaceCleanedEvent(space.id)
-      console.log(space, last_cleaned_event)
       counts(space.id, last_cleaned_event.time.toISOString(), demo_now().toISOString())
       .then(
         counts_resp => {
           let count = 0
           counts_resp.data.results.forEach(result=> count += result.interval.analytics.entrances)
           let _spaces = this.state.spaces.filter(value => value.id !== space.id )
-          _spaces = [..._spaces, {id: space.id, name: space.name, count: count}]
+          _spaces = [..._spaces, {
+              id: space.id, 
+              name: space.name,
+              count: count,
+              cleaningInProgress:last_cleaned_event.cleaningInProgress,
+              start_time: last_cleaned_event.start_time
+             }]
           _spaces.sort((a, b) => (a.count > b.count) ? -1 : 1)
           this.setState({spaces: _spaces})
         }
@@ -48,7 +60,7 @@ class App extends Component{
   fetchEntrCountsInSpaces(){
     spaces()
     .then(resp => {
-        resp.data.results.forEach(space => { this.fetchEntrCounts(space)});
+        resp.data.results.forEach(space => { if (space.space_type !== "building"){ this.fetchEntrCounts(space) };});
     });
   }
 
@@ -67,12 +79,22 @@ class App extends Component{
     this.fetchEntrCounts(_space);
   }
 
+  startCleaning(space_id){
+    let event = this.addStartCleaningEvent(space_id);
+    let _spaces = [...this.state.spaces]
+    let _space_index = -1
+    _spaces.forEach((space, i) => _space_index = (space.id === space_id)?i:_space_index )
+    _spaces[_space_index] = {..._spaces[_space_index], start_time: event.start_time , cleaningInProgress: event.cleaningInProgress }
+    this.setState({spaces: _spaces})
+  }
+
   render(){
     return (
       <div className="App">
         <br/>
         <Container>
-          <SpacesCards spaces={this.state.spaces} onSpaceCleared={ id => this.spaceCleared(id) }/>
+          <h2>Hight touch surface cleaning</h2>
+          <SpacesCards spaces={this.state.spaces} onSpaceCleared={ id => this.spaceCleared(id) } onStartCleaning={ id => this.startCleaning(id)}/>
         </Container>
       </div>
     );
--- src/components/spaceCards.js
+++ src/components/spaceCards.js
@@ -1,24 +1,40 @@
 import React, {Component} from 'react';
 import {Accordion, Card, Button, } from 'react-bootstrap';
+import { demo_now } from '../services/demoTimeService';
 
 class SpaceCards extends Component{
+    cleaningDuration(start_time){
+      let diff = demo_now().getTime() - start_time.getTime();
+      let hours = Math.floor(diff / (60*60*1000))
+      let hours_str = hours.toString().padStart(2, "0")
+      let min = Math.floor((diff%(60*60*1000))/(60*1000))
+      let min_str = min.toString().padStart(2, "0")
+      let sec = Math.floor((diff%(60*1000))/1000)
+      let sec_str = sec.toString().padStart(2, "0")
+      return `${hours_str}:${min_str}:${sec_str}`
+    }
     render()
     {
-        const { spaces, onSpaceCleared } = this.props;
+        const { spaces, onSpaceCleared, onStartCleaning } = this.props;
         return (
         <Accordion defaultActiveKey="0">
             {
                 spaces.map( (space, i) => (
                     <Card>
                     <Card.Header>
-                      <Accordion.Toggle as={Button} variant="link" eventKey={i}>
+                      <Accordion.Toggle as={Button} variant={(space.count<=20)?"info":(space.count<=50)?"warning":"danger"} eventKey={i}>
                         {space.name}
-                      </Accordion.Toggle> <Card.Text>Entrances: {space.count}</Card.Text>
+                      </Accordion.Toggle> <Card.Text>Visits: {space.count}</Card.Text>
                     </Card.Header>
                     <Accordion.Collapse eventKey={i}>
                         <Card.Body>
-                          Hello! I'm the body with id {space.id}
-                          <Button variant="success" areaId={space.id} onClick={()=> onSpaceCleared(space.id)} >Area Cleard</Button>
+                          <Button 
+                            variant={(space.cleaningInProgress)?"light":"primary"}
+                            disabled={space.cleaningInProgress}
+                            onClick={()=>onStartCleaning(space.id)}>
+                              {(space.cleaningInProgress)?this.cleaningDuration(space.start_time):"Start Cleaning"}
+                            </Button>&nbsp;
+                            <Button variant="success" areaId={space.id} onClick={()=> onSpaceCleared(space.id)} >Cleaned</Button>
                         </Card.Body>
                     </Accordion.Collapse>
                     </Card>
